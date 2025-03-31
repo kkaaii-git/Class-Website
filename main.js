5821249a2e1664d98d632f6e127b081eaea700c9
mujohannes <61717026+mujohannes@users.noreply.github.com>
2025-03-31 23:16:04 +0000

    opening and closing the menu

--- main.js
+++ main.js
@@ -6,7 +6,12 @@ function onLoad() {
     const nav = document.querySelector(".main-navigation")
     // add a listener to btn for click
     btn.addEventListener( "click" , function() {
-        
+        if( nav.classList.contains("open") ) {
+            nav.classList.remove("open")
+        }
+        else {
+            nav.classList.add("open")
+        }
     })
 }
 // waiting for "load" event to happen and execute onLoad
