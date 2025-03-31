9372e3c83b4cfb3f7e97dfd0f8f6459239eb2b33
mujohannes <61717026+mujohannes@users.noreply.github.com>
2025-03-31 23:11:59 +0000

    adding javascript for navigation

---
+++ main.js
@@ -0,0 +1,13 @@
+// Javascript
+// function definition for onLoad
+function onLoad() {
+    // reference to nav button
+    const btn = document.querySelector(".nav-button")
+    const nav = document.querySelector(".main-navigation")
+    // add a listener to btn for click
+    btn.addEventListener( "click" , function() {
+        
+    })
+}
+// waiting for "load" event to happen and execute onLoad
+window.addEventListener( "load", onLoad )
